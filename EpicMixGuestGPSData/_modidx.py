f02149f13752e49db8099cc2d0bd5f790331aadd
Jeremy Demlow <jeremydemlow@Jeremys-MacBook-Pro.local>
2022-10-07 14:40:33 -0700

    db movement again lets see

--- EpicMixGuestGPSData/_modidx.py
+++ EpicMixGuestGPSData/_modidx.py
@@ -6,4 +6,6 @@ d = { 'settings': { 'branch': 'main',
                 'git_url': 'https://github.com/JeremyDemlow/EpicMixGuestGPSData',
                 'lib_path': 'EpicMixGuestGPSData'},
   'syms': { 'EpicMixGuestGPSData.core': { 'EpicMixGuestGPSData.core.snowflake_poc_table_grab': ( 'gps_poc.html#snowflake_poc_table_grab',
-                                                                                                 'EpicMixGuestGPSData/core.py')}}}
+                                                                                                 'EpicMixGuestGPSData/core.py')},
+            'EpicMixGuestGPSData.corez': { 'EpicMixGuestGPSData.corez.snowflake_poc_table_grab': ( 'gps_poc.html#snowflake_poc_table_grab',
+                                                                                                   'EpicMixGuestGPSData/corez.py')}}}
