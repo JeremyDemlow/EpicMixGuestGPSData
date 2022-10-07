72f0d9a0f4e6251390479e8924dc47f7ec29da2c
Jeremy Demlow <jeremydemlow@Jeremys-MacBook-Pro.local>
2022-10-07 11:18:26 -0700

    quick test

--- EpicMixGuestGPSData/_modidx.py
+++ EpicMixGuestGPSData/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://JeremyDemlow.github.io',
                 'git_url': 'https://github.com/JeremyDemlow/EpicMixGuestGPSData',
                 'lib_path': 'EpicMixGuestGPSData'},
-  'syms': {'EpicMixGuestGPSData.core': {'EpicMixGuestGPSData.core.foo': ('core.html#foo', 'EpicMixGuestGPSData/core.py')}}}
+  'syms': { 'EpicMixGuestGPSData.core': { 'EpicMixGuestGPSData.core.snowflake_poc_table_grab': ( 'gps_poc.html#snowflake_poc_table_grab',
+                                                                                                 'EpicMixGuestGPSData/core.py')}}}
