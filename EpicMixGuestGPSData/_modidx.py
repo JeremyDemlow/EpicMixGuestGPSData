80f277511a2ec9ca298a8a993106daf7e5b72bf2
Jeremy Demlow <jeremydemlow@Jeremys-MacBook-Pro.local>
2022-10-14 12:16:10 -0600

    change to settle the creation of snowflake data asset

--- EpicMixGuestGPSData/_modidx.py
+++ EpicMixGuestGPSData/_modidx.py
@@ -5,7 +5,9 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://JeremyDemlow.github.io',
                 'git_url': 'https://github.com/JeremyDemlow/EpicMixGuestGPSData',
                 'lib_path': 'EpicMixGuestGPSData'},
-  'syms': { 'EpicMixGuestGPSData.core': { 'EpicMixGuestGPSData.core.snowflake_poc_table_grab': ( 'gps_poc.html#snowflake_poc_table_grab',
-                                                                                                 'EpicMixGuestGPSData/core.py')},
-            'EpicMixGuestGPSData.corez': { 'EpicMixGuestGPSData.corez.snowflake_poc_table_grab': ( 'gps_poc.html#snowflake_poc_table_grab',
-                                                                                                   'EpicMixGuestGPSData/corez.py')}}}
+  'syms': { 'EpicMixGuestGPSData.poc_gps': { 'EpicMixGuestGPSData.poc_gps.create_circle_area_around_lifts': ( 'gps_poc.html#create_circle_area_around_lifts',
+                                                                                                              'EpicMixGuestGPSData/poc_gps.py'),
+                                             'EpicMixGuestGPSData.poc_gps.kml2csv': ( 'gps_poc.html#kml2csv',
+                                                                                      'EpicMixGuestGPSData/poc_gps.py'),
+                                             'EpicMixGuestGPSData.poc_gps.process_coordinate_string': ( 'gps_poc.html#process_coordinate_string',
+                                                                                                        'EpicMixGuestGPSData/poc_gps.py')}}}
